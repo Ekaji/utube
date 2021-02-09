8239ea06645cbe63598adcdd4351fd19daadc3a0
Ekaji <ekajionah@gmail.com>
2021-02-09 08:48:29 +0100

    added function mergeArrayObject to merge Objects Array

--- src/App.vue
+++ src/App.vue
@@ -24,43 +24,109 @@ export default {
     data() {
         return {
             key: process.env.VUE_APP_YOUTUBE_CLONE_API_KEY ,
-            searchResult: [],
+            searchResut: [],
+            videoIDs: [],
             videoStats: [],
-            allVideoId: [],
+            mergedData: [],
+            allVideoId: []
     }
 },
+   
 
   methods: {
     search(searchFor) {
-    console.log(searchFor)
+    console.log(searchFor);
      axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&order=relevance&q=${searchFor}&key=${this.key}`)
       .then(res => {
-        this.searchResult = [...res.data.items];
-       let Result = this.searchResult.map(result => ({
+        let searchResponse = [...res.data.items];
+        this.searchResult = searchResponse.map(result => ({
             id : result.id.videoId , title : result.snippet.description , image : result.snippet.thumbnails.medium.url 
-        }))
-      
-        console.log(Result.id );
-        this.getStats();
+        }),
+    );
+
+      this.videoIDs = this.searchResult.map(result => {
+            return result.id
+        },
+    );
+        console.log('sr ', this.searchResult, 'ids ', this.videoIDs);
+        this.getStats(this.videoIDs);
       },[])
     },
 
 // returns the statistics of every video
-    getStats(){
-  axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=${[...this.searchResult]}&key=${this.key}`)
+  getStats(ids){
+     axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=${ids}&key=${this.key}`)
         .then(res => {
-        this.videoStats = res.data.items;
-        console.log(this.videoStats)
+        let Stats = [...res.data.items];
+
+        this.videoStats = Stats.map(stats => ({
+            id : stats.id , comment: stats.statistics.commentCount, dislike: stats.statistics.dislikeCount, like: stats.statistics.likeCount,
+            views: stats.statistics.viewCount
         })
-    }
+    );
+
+        console.log('vidstats ', this.videoStats)
+
+        this.mergeArrayObjects(this.videoStats, this.searchResult);
+        // console.log(this.mergeArrayObjects(this.videoStats, this.searchResult));
+
+        },
+        
+        )
+
   },
+
+  mergeArrayObjects(arr1,arr2){
+  this.mergedData = arr1.map((item,i)=>{
+     if(item.id === arr2[i].id){
+         //merging two objects
+       return Object.assign({}, item, arr2[i])
+     }
+  }
+  
+  );
+  console.log(this.mergedData)
+}
+
+ 
+//     mergedById(a1, a2){
+//    this.mergedData = a1.map(itm => ({
+//         ...a2.find((item) => (item.id === itm.id) && item),
+//         ...itm
+//         }
+//     ),
+//     console.log('merged ', this.mergedData)
+// );
+
+// },
+  
+
+//   mergedResponse(){
+//     //   thi mergedData = [];
+//       for(let i=0; i < this.searchResut.length; i++) {
+//         this.mergedData.push({
+//          ...this.searchResut[i],
+//          ...(this.videoStats.find((itmInner) => itmInner.id === this.searchResut[i].id))
+//          },
+
+//   );
+//   console.log('merged ', this.mergedData)
+// console.log('response')
+
+// }
+
+//   },
+
+
+ },
+
+
 
  mounted() {
 //default search paramiter to mimick youtube video recomendation on start
      this.search('cars')
  },
 }
- 
 
 
 </script>
